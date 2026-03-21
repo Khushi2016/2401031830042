4ecc7a54bd9ef4836dd1318509cbf3c92eef0c27
Khushi2016 <shahkhushi170@gmail.com>
2026-03-21 22:33:31 +0530

    Update script.js

--- Code/script.js
+++ Code/script.js
@@ -1,3 +1,15 @@
-function showMessage() {
-  alert("Welcome to CodeMentor AI!");
+function getSuggestion() {
+  let score = Math.floor(Math.random() * 100);
+
+  let suggestion = "";
+
+  if (score < 40) {
+    suggestion = "Start with HTML Basics";
+  } else if (score < 70) {
+    suggestion = "Practice CSS and Layouts";
+  } else {
+    suggestion = "Try Advanced JavaScript Challenges";
+  }
+
+  document.getElementById("suggestion").innerText = suggestion;
 }
